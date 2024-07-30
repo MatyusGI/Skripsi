16d11e7d237373686708e6940c7253b521c21e44
MatyusGI <matyus.g.ismanto25@gmail.com>
2024-07-30 11:25:34 +0700

    vim param

--- preprocessing_project_copy.py
+++ preprocessing_project_copy.py
@@ -1197,8 +1197,8 @@ def main():
 
     model, loss_values, correlation_values, num_epochs, time = training_vim(train_x, train_y)
     test_mse, test_corr, outputs_flat, targets_flat = test_vim(model, test_x, test_y)
-    plot_vim(loss_values, correlation_values, num_epochs, time, name='training_performance_vim_100_epoch_1')
-    plot_test_results(outputs_flat, targets_flat, test_corr, test_mse, name='test_performace_vim_100_epoch_1')
+    plot_vim(loss_values, correlation_values, num_epochs, time, name='training_performance_vim_100_epoch_0')
+    plot_test_results(outputs_flat, targets_flat, test_corr, test_mse, name='test_performace_vim_100_epoch_0')
 
 
     # # Set CUDA_LAUNCH_BLOCKING to help with debugging
