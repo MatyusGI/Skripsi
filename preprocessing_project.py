22cc016c97d6258c4cdda50e6135c55f3b3888c2
MatyusGI <matyus.g.ismanto25@gmail.com>
2024-08-13 01:22:44 +0700

    update parameter

--- preprocessing_project.py
+++ preprocessing_project.py
@@ -872,16 +872,16 @@ def training_vim_test(train_x, train_y, test_x, test_y, epoch, name):
 
     # Initialize the Vim model
     model = Vim(
-        dim=80,
+        dim=128,
         dt_rank=16,
-        dim_inner=80,
-        d_state=64,
+        dim_inner=128,
+        d_state=97,
         num_classes=1,  # For regression, typically the output is a single value per instance
         image_size=286,
         patch_size=13,
         channels=1,
-        dropout=0.2,
-        depth=5,
+        dropout=0.2677301595791723,
+        depth=7,
     )
 
     # Move the model to the GPU
@@ -889,7 +889,7 @@ def training_vim_test(train_x, train_y, test_x, test_y, epoch, name):
 
     # Using Mean Squared Error Loss for a regression task
     criterion = MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.0001, weight_decay=1e-03)
+    optimizer = optim.Adam(model.parameters(), lr=0.00015334092031733988, weight_decay=1e-03)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, verbose=True)
 
     # Training loop
