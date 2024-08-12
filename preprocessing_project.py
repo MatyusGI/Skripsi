a75e57a752cb8ec1ab52884b7f3726b1e3ab66ab
MatyusGI <matyus.g.ismanto25@gmail.com>
2024-08-13 04:25:49 +0700

    update parameter

--- preprocessing_project.py
+++ preprocessing_project.py
@@ -889,7 +889,7 @@ def training_vim_test(train_x, train_y, test_x, test_y, epoch, name):
 
     # Using Mean Squared Error Loss for a regression task
     criterion = MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.00001, weight_decay=1e-03)
+    optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-03)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, verbose=True)
 
     # Training loop
